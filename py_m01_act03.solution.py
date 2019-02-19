acb26366e48ecad88a7d1279c7bae8a4d52aa235
k2controls <kkelly@k2controls.net>
2019-02-18 22:07:20 -0500

    Update py_m01_act03.solution.py

--- py_m01_act03.solution.py
+++ py_m01_act03.solution.py
@@ -1,4 +1,5 @@
 # Python Module 01: Activity 02
+from time import sleep
 from iot_gh.IoTGreenhouseService import IoTGreenhouseService
 
 ghs = IoTGreenhouseService()
@@ -13,6 +14,7 @@ while ghs.switches.push_button.is_off():
     pass
 for i in range(0, key_num):
     ghs.buzzer.beep()
+    sleep(.5)
 
 key_num = 2
 initial_pot_val = ghs.analog.pot.get_value()
@@ -22,15 +24,17 @@ while new_pot_value <= initial_pot_val + 10:
     new_pot_value = ghs.analog.pot.get_value()
 for i in range(0, key_num):
     ghs.buzzer.beep()
+    sleep(.5)
 
 key_num = 3
 initial_light_val = ghs.analog.light.get_value()
 new_light_value = 1024
 print("Activate Key", key_num)
-while new_light_value > initial_light_val - 100:
+while new_light_value > initial_light_val - 10:
     new_light_value = ghs.analog.light.get_value()
 for i in range(0, key_num):
     ghs.buzzer.beep()
+    sleep(.5)
 
 key_num = 4
 print("Activate Key", key_num)
@@ -38,15 +42,17 @@ while ghs.switches.toggle.is_off():
     pass
 for i in range(0, key_num):
     ghs.buzzer.beep()
+    sleep(.5)
 
 key_num = 5
-current_temp = ghs.temperature.get_external_temp_C()
+current_temp = ghs.temperature.get_outside_temp_C()
 new_temp_value = 0
 print("Activate Key", key_num)
 while new_temp_value <= current_temp + 1:
-    new_temp_value = ghs.temperature.get_external_temp_C()
+    new_temp_value = ghs.temperature.get_outside_temp_C()
 for i in range(0, key_num):
     ghs.buzzer.beep()
+    sleep(.5)
 
 #success - open greenhouse
 print("Keys entered. Greenhouse open!")
