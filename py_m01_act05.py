18010fd7f949d413cf399076da2b88f13ea882c9
k2controls <kkelly@k2controls.net>
2019-02-25 03:28:57 -0500

    Revised act05

--- py_m01_act05.py
+++ py_m01_act05.py
@@ -1,65 +1,22 @@
+''' IoT Greenhouse - Module 1: Activity 05
+    [your name]
+
+    Sets temperature setpoint using potentiometer and push-button
+'''
 from time import sleep
 from iot_gh.IoTGreenhouseService import IoTGreenhouseService
 
 ghs = IoTGreenhouseService()
-number = ghs.greenhouse.house_number
 
-print("IoT Greenhouse - Python Introduction.")
-print("House Number: " + number)
+print("IoT Greenhouse - Set Temperature Threshold")
 print()
 
-print("Greenhouse controller")
-print()
+#wait for pot in center position
 
-print("Turn potentiometer fully counter-clockwise.")
-pot_value = ghs.analog.pot.get_value()
-while pot_value > 0:
-    pot_value = ghs.analog.pot.get_value()
+#pot good - get initial temp 
 
-current_temp = ghs.temperature.get_inside_temp_F()
-print("Current internal temperature is " + str(current_temp))
-print("Turn pot to set threshold temperature.")
-print("Press push-button switch when done.")
-TEMP_SPAN = 10
-MAX_POT_VAL = 1023
-old_threshold = current_temp
-threshold = current_temp
-while ghs.switches.push_button.is_off():
-    pot_value = ghs.analog.pot.get_value()
-    pot_percent = pot_value/MAX_POT_VAL
-    delta_temp = TEMP_SPAN * pot_percent
-    threshold = round(current_temp + delta_temp, 1)
-    if threshold != old_threshold:
-        print(threshold, end=" ")
-        old_threshold = threshold
-    sleep(.5)
-print("\nThreshold temp set to " + str(threshold) + "\n")
-print("Turn potentiometer fully counter-clockwise.")
-while ghs.analog.pot.get_value() > 0:
-    pass
-print()
-pos_index = 1
-# Can you create code that uses the pot to set pos_index,
-# an open position for the louver that is less than 1?
-#--your extension here --
+#loop while no push button press
+#get pot value, calculate new setpoint, and display
 
-print()
-#reset servo position
-ghs.servo.move(0)
-status = "CLOSED"
-#monitoring temperature and updating louver
-print("Monitoring...")
-while True:
-    tempF = ghs.temperature.get_inside_temp_F()
-    print("temp = " + str(tempF))
-    if tempF > threshold and status == "CLOSED":
-        print("opening")
-        ghs.servo.move(pos_index)
-        status = "OPEN"
-    elif tempF < threshold and status == "OPEN":
-        print("closing")
-        ghs.servo.move(0)
-        status = "CLOSED"
-    sleep(5)
-
-
+#loop done - display threshold value
+print("Threshold temp set to", threshold)
--- py_m01_act05.solution.py
+++ py_m01_act05.solution.py
@@ -1,23 +1,30 @@
+''' IoT Greenhouse - Module 1: Activity 05
+    Keith E. Kelly
+    K2 Creatives, LLC
+
+    Sets temperature setpoint using potentiometer and push-button
+'''
 from time import sleep
 from iot_gh.IoTGreenhouseService import IoTGreenhouseService
 
 ghs = IoTGreenhouseService()
-number = ghs.greenhouse.house_number
 
-print("IoT Greenhouse - Python Introduction.")
-print("House Number: " + number)
+print("IoT Greenhouse - Set Temperature Threshold")
 print()
 
-print("Greenhouse controller")
-print()
-
-print("Turn potentiometer fully counter-clockwise.")
+#check for pot in center position
 pot_value = ghs.analog.pot.get_value()
-while pot_value > 0:
+if pot_value != 512:
+    print("Please center potentiometer.")
+    ghs.lamps.red.on()
+while pot_value != 512:
     pot_value = ghs.analog.pot.get_value()
+    sleep(.2)
 
+#Pot good - start 
+ghs.lamps.red.off()
 current_temp = ghs.temperature.get_inside_temp_F()
-print("Current internal temperature is " + str(current_temp))
+print("Current internal temperature is", current_temp)
 print("Turn pot to set threshold temperature.")
 print("Press push-button switch when done.")
 TEMP_SPAN = 10
@@ -34,39 +41,3 @@ while ghs.switches.push_button.is_off():
         old_threshold = threshold
     sleep(.5)
 print("\nThreshold temp set to " + str(threshold) + "\n")
-print("Turn potentiometer fully counter-clockwise.")
-while ghs.analog.pot.get_value() > 0:
-    pass
-print()
-pos_index = 1
-# Can you create code that uses the pot to set pos_index,
-# an open position for the louver that is less than 1?
-#--your extension here --
-print("Turn pot to set servo open position.")
-print("Press push-button switch when done.")
-while ghs.switches.push_button.is_off():
-    pot_value = ghs.analog.pot.get_value()
-    pos_index = pot_value/MAX_POT_VAL
-    ghs.servo.move(pos_index)
-    sleep(.2)
-open_percent = round(pos_index * 100)
-print("\nLouver open position set to " + str(open_percent) + "%\n\n")
-#reset servo position
-ghs.servo.move(0)
-status = "CLOSED"
-#monitoring temperature and updating louver
-print("Monitoring...")
-while True:
-    tempF = ghs.temperature.get_inside_temp_F()
-    print("temp = " + str(tempF))
-    if tempF > threshold and status == "CLOSED":
-        print("opening")
-        ghs.servo.move(pos_index)
-        status = "OPEN"
-    elif tempF < threshold and status == "OPEN":
-        print("closing")
-        ghs.servo.move(0)
-        status = "CLOSED"
-    sleep(5)
-
-
